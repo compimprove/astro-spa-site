4e72622d3147e75a9053d304e543302248502504
DinhNT <compimprove@gmail.com>
2024-10-07 08:47:09 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,22 @@
 import { defineConfig } from "astro/config";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
+import { loadEnv } from "vite";
+const { PUBLIC_ENV } = loadEnv(import.meta.env.MODE, process.cwd(), "");
 
 import cloudflare from "@astrojs/cloudflare";
 
+const isPreview = PUBLIC_ENV === "Preview";
+
 // https://astro.build/config
 export default defineConfig({
-  output: "hybrid",
+  output: isPreview ? "server" : "hybrid",
   adapter: cloudflare(),
   integrations: [
     sanity({
       projectId: "wx9xhwt1",
       dataset: "production",
-      useCdn: false,
+      useCdn: isPreview,
       // See note on using the CDN
       apiVersion: "2024-07-24",
       // insert the current date to access the latest version of the API
